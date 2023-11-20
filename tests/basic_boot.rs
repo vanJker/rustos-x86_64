b1ebf492b964074246296ff45fde3c8c7a9f9478
vanJker <1847722163@qq.com>
2023-11-21 00:01:02 +0800

    🚀 #[004 Testing]

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,30 +7,19 @@
 use core::panic::PanicInfo;
 use rustos_x86_64::println;
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
     loop {}
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rustos_x86_64::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello, World{}", "!");
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
+fn test_println() {
+    println!("test_prinltn output");
 }
